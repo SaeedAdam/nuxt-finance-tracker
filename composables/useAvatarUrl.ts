a242caff0153fb5258603f229faf3a64fad89892
SaeedAdam <sandeed70@gmail.com>
2024-01-20 16:07:16 +0300

    fix typo in useAvatarUrl.ts

--- composables/useAvatarUrl.ts
+++ composables/useAvatarUrl.ts
@@ -1,6 +1,6 @@
 export const useAvatarUrl = () => {
   // Get the current user metadata - which contain name of avatar image
-  // Public URL of that image
+  // Get Public URL of that image
   // Watch user for any changes - so we always display image of the user
 
   const supabase = useSupabaseClient()
