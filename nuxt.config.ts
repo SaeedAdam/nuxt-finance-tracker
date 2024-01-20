900131a7cc5df19c68546907edd1e2c9eca074e7
SaeedAdam <sandeed70@gmail.com>
2024-01-20 17:09:23 +0300

    .

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ['@nuxt/ui', '@nuxtjs/supabase'],
   supabase: {
-    redirect: true
+    redirect: false
   },
   runtimeConfig: {
     public:{
